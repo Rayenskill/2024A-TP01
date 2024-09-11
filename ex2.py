c0d371326d58cd4afeb6bae6cd1a268f825a002d
Rayenskill <raybour9@gmail.com>
2024-09-11 16:57:57 -0400

    J'ai corrige un bug

--- ex2.py
+++ ex2.py
@@ -8,7 +8,7 @@ water_quantity = float(x)
 if water_quantity < 5:
     print("La quantité d'eau est trop petite")
 
-nb_filtre = water_quantity // 5
-nb_lampes = 3 * water_quantity // 5
-chlore = 0,5 * water_quantity // 5
+nb_filtre = int(water_quantity // 5)
+nb_lampes = int(3 * water_quantity // 5)
+chlore = 0.5 * water_quantity // 5
 print(f"Il faut {nb_filtre} de filtre, {nb_lampes} de lampes et {chlore}kg de chlore pour assainir {water_quantity}L d'eau")
