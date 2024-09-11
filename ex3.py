0d67bd96728bc23d3b5e23b5c746c1fe6001a9ad
Rayenskill <raybour9@gmail.com>
2024-09-11 17:38:42 -0400

    Excersice terminé

--- ex3.py
+++ ex3.py
@@ -5,5 +5,5 @@ import math
 speed = float(input("Quelle est la vitesse initiale de la boule ? ")) 
 angle = float(input("Quel est l'angle de lancement de la boule ? "))
 g = 9.8
-distance = ((speed ** 2)*(math.sin(2*angle)))/g
-print(f"La distance maximale parcourue par la balle est de {distance:.3}m",)
+distance = ((speed ** 2)*(math.sin(2*((angle*math.pi)/180))))/g
+print(f"La distance maximale qui peut être parcourue par la balle est de {distance:.4}m",)
