73374649ebc72d4a5a21659388dc177bcd313e90
Rayenskill <raybour9@gmail.com>
2024-09-13 10:26:28 -0400

    FINI

--- ex4.py
+++ ex4.py
@@ -3,20 +3,19 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = int(input("Quel est le le pourcentage de votre batterie ? "))
+battery_level = float(input("Veuillez entrer le pourcentage de batterie (entre 0 et 100) : "))
 
-if battery_level in range(51, 101):
-    distance = (battery_level - 50) * 2 + ((battery_level - 25) / 2) + (battery_level - 10) + (battery_level - 5) * 2.5 + (battery_level * 6)
-    print(f"la distance maximale que vous pouvez parcourir est de {round(distance, 1)}km")
+if battery_level in range(51,101):
+    distance = ((battery_level - 50) * 2) + (25 * 0.5) + (15 * 1) + (5 * 2.5) + (5 * 6)
 elif battery_level in range(26, 51):
-    distance = ((battery_level - 25) / 2) + (battery_level - 10) + (battery_level - 5) * 2.5 + (battery_level * 6)
-    print(f"la distance maximale que vous pouvez parcourir est de {round(distance, 1)}km")
+    distance = ((battery_level - 25) * 0.5) + (15 * 1) + (5 * 2.5) + (5 * 6)
 elif battery_level in range(11, 26):
-    distance = battery_level 
-    print(f"la distance maximale que vous pouvez parcourir est de {round(distance, 1)}km")
+    distance = ((battery_level - 10) * 1) + (5 * 2.5) + (5 * 6)
 elif battery_level in range(6, 11):
-    distance = battery_level * 2.5
-    print(f"la distance maximale que vous pouvez parcourir est de {round(distance, 1)}km")
+    distance = ((battery_level - 5) * 2.5) + (5 * 6)
 elif battery_level in range(0, 6):
-    distance = battery_level * 6
-    print(f"la distance maximale que vous pouvez parcourir est de {round(distance, 1)}km")
+    distance = (battery_level * 6)
+else:
+    print("la valeur entrée est invalide")
+
+print(f"La distance maximale que vou pouvez parcourir est de {round(distance, 1)}km.")
