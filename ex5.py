3c159982d76f6d4814a109edceb77dcc341d6c61
Rayenskill <raybour9@gmail.com>
2024-09-13 10:02:29 -0400

    Excersice terminé

--- ex5.py
+++ ex5.py
@@ -1,5 +1,16 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+country = input("Entrez le pays pour lequel vous voulez entrer les médailles ")
+
+while not country.isalpha():
+    print("Vous avez entré une chaine de caractères invalide pour le pays \nVeuillez entrer les lettres représentant votre pays ")
+    country = input("Entrez le pays pour lequel vous voulez entrer les médailles ")
+    
+
+code_medals = input("Entrer la chaine de caratère représentant les médailles gagnées ")
+code_medals.upper()
+gold = code_medals.count("G")
+silver = code_medals.count("S")
+bronze = code_medals.count("B")
+print(f"MÉDAILLES : \n Nombre de médailles d'or : {gold} \n Nombre de médailles d'argent : {silver} \n Nombre de médailles de bronze : {bronze}")
